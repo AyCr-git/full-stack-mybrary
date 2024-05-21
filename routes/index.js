c46fb614a1cdaecae61abd56cbe38fdfb2a69fd2
AyCr <aycr@AyCrs-MacBook-Pro.local>
2024-05-21 11:54:18 +0100

    Initial Setup

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res)=> {
+    //res.send('Hello World')
+    res.render('index')
+})
+
+
+module.exports = router
