b7036356f35b5648e01bcc422c2d5f93440a33dc
AyCr <aycr@AyCrs-MacBook-Pro.local>
2024-05-21 18:57:12 +0100

    Inial Author Routers

---
+++ routes/authors.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const router = express.Router()
+const Author = require('../models/author')
+const author = require('../models/author')
+
+//All Authors Route
+router.get('/', async (req, res) => {
+
+    const searchOptions = {}
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+
+
+
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', { 
+            authors: authors,
+            searchOptions: req.query
+        })
+    
+    } catch (error) {
+        res.redirect("/")
+    }
+})
+
+//New Author Route
+router.get('/new', (req, res) => {
+    res.render('authors/new', { author: new Author() })
+})
+
+//Create Author Route
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+
+    try {
+        const newAuthor = await author.save()
+        //res.redirect(`authors/${newAuthor.id}`)
+        res.redirect(`authors`)
+    } catch (error) {
+        res.render('authors/new',  {
+            author: author,
+            errorMessage: 'Error creating author'
+        })
+    }
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -6,8 +6,10 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
-const routerIndex = require('./routes/index')
+const indexRouter = require('./routes/index')
+const authorsRouter = require('./routes/authors')
 
 
 app.set('view engine', 'ejs')
@@ -15,6 +17,8 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL) //, {useNewUrlParser: true}
@@ -24,7 +28,8 @@ db.once('open', () => console.log('Connected to mongoose'))
 
 
 
-app.use('/', routerIndex)
+app.use('/', indexRouter)
+app.use('/authors', authorsRouter)
 
 
 app.listen(process.env.PORT || 3000)
